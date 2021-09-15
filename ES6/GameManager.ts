eabd029069c2908fe0ca5c02e154337b6d6b8ba3
hyo-choi <hyochoi31@gmail.com>
2021-09-15 19:48:26 +0900

    Fix: update를 메소드에서 일반 프로퍼티로 수정

--- ES6/GameManager.ts
+++ ES6/GameManager.ts
@@ -5,12 +5,11 @@ const INTERVAL = 10;
 export default class GameManager {
   roomManager: RoomManager;
 
+  update: NodeJS.Timer ;
+
   constructor(roomManager: RoomManager) {
     this.roomManager = roomManager;
-  }
-
-  update(): void {
-    setInterval(() => {
+    this.update = setInterval(() => {
       const keys = Object.keys(this.roomManager.rooms);
       keys.forEach((key) => {
         this.roomManager.rooms[key].loop();
